51d4876a00e43516ee02640ec337f0d136629bd0
Stevie <mail@steviegoodman.com>
2023-01-23 21:23:39 +0000

    Added `MovementConfig` ScriptableObject

---
+++ Assets/Scripts/Characters/MovementConfig.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace StealthPrototype.Characters
+{
+    [CreateAssetMenu(fileName = "Movement Configuration", menuName = "Characters/Movement Configuration", order = 0)]
+    public class MovementConfig : ScriptableObject
+    {
+        public float speed;
+    }
+}
